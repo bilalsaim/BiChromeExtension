d38c2fb63c96681a4c3bde3e0336695713b37131
Bilal Saim <info@bilalsaim.com>
2020-01-30 23:43:46 +0100

    Add extra

--- js/extrafield.js
+++ js/extrafield.js
@@ -1,12 +1,26 @@
-var te = $("#tsf");
 
-var url = "http://";
 
-chrome.runtime.sendMessage({method: "getUrlToConnect"}, function(response) {
-    console.log(response.urlToConnect);
-    url = response.urlToConnect;
+$(document).ready(function()
+{
+    var te = $("#tsf");
+
+    var url = "http://";
+
+    chrome.runtime.sendMessage({method: "getUrlToConnect"}, function(response) {
+        console.log(response.urlToConnect);
+        url = response.urlToConnect;
+
+        te.append("<input type='text' value='"+url+"'/>");
+    });
 
     te.append("<input type='text' value='"+url+"'/>");
+
+    te.append("<select class=\"js-example-basic-multiple\" name=\"states[]\" multiple=\"multiple\">\n" +
+        "  <option value=\"AL\">Alabama</option>\n" +
+        "  <option value=\"WY\">Wyoming</option>\n" +
+        "</select>");
+
+
+    $('.js-example-basic-multiple').select2();
 });
 
-te.append("<input type='text' value='"+url+"'/>");
